05953cf9af85a908d15cba4738ece4ee2f6cd08d
CanvasL <746591811@qq.com>
2024-03-27 14:36:35 +0800

    fix: path error

--- idos-runtime/contracts/script/06_DeployDataverseClientMock.s.sol
+++ idos-runtime/contracts/script/06_DeployDataverseClientMock.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import {DataverseClientMock} from "../../test/extension/mocks/DataverseClientMock.sol";
+import {DataverseClientMock} from "../test/mocks/DataverseClientMock.sol";
 import "forge-std/Script.sol";
 
 contract DeployDataverseClientMock is Script {
